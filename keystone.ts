ddb84d6773bbbdb96f1788d4e78ca0649e797872
ali-opd <nurali.batamindo@gmail.com>
2022-03-28 18:29:32 +0700

    Initial commit

---
+++ keystone.ts
@@ -0,0 +1,60 @@
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {},
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test,
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    // TODO: Add session value here
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
